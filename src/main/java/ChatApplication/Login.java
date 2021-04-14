1fd7bb3aa501e289196484be7fa4d98228b84635
Eetu <eetu.vaananen@hotmail.com>
2021-04-14 17:04:48 +0300

    Minor improvements

--- src/main/java/ChatApplication/Login.java
+++ src/main/java/ChatApplication/Login.java
@@ -1,6 +1,8 @@
 package ChatApplication;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 
@@ -11,13 +13,24 @@ public class Login extends JFrame {
 
     public Login() {
         initComponents();
+        centeredFrame(loginFrame);
+    }
+
+    // Center window
+    private void centeredFrame(javax.swing.JFrame jFrame) {
+        Dimension objDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int iCoordX = (objDimension.width - jFrame.getWidth()) / 2;
+        int iCoordY = (objDimension.height - jFrame.getHeight()) / 2;
+        jFrame.setLocation(iCoordX, iCoordY);
     }
 
     private void initComponents() {
+        // Get an instance of authentication class
         Authentication authentication = Authentication.getInstance();
 
         loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        loginFrame.setSize(850, 700);
+        loginFrame.setSize(1000, 800);
+        loginFrame.setResizable(false);
 
         loginFrame.add(loginPanel);
         loginPanel.setLayout(null);
@@ -27,43 +40,43 @@ public class Login extends JFrame {
         // Create all components for login window
         JLabel titleLabel = new JLabel("Kirjautuminen");
         titleLabel.setFont(new java.awt.Font("Dialog", 1, 44));
-        titleLabel.setBounds(275, 20, 300, 100);
+        titleLabel.setBounds(350, 20, 300, 100);
         titleLabel.setForeground(textColor);
 
         JLabel usernameLabel = new JLabel("Käyttäjänimi");
-        usernameLabel.setBounds(275, 190, 100, 25);
+        usernameLabel.setBounds(350, 190, 100, 25);
         usernameLabel.setForeground(textColor);
         usernameLabel.setFont(new java.awt.Font("Segoe UI", 1, 14));
 
         JTextField usernameField = new JTextField();
-        usernameField.setBounds(275, 220, 300, 45);
+        usernameField.setBounds(350, 220, 300, 45);
 
         JLabel passwordLabel = new JLabel("Salasana");
-        passwordLabel.setBounds(275, 265, 100, 25);
+        passwordLabel.setBounds(350, 265, 100, 25);
         passwordLabel.setForeground(textColor);
         passwordLabel.setFont(new java.awt.Font("Segoe UI", 1, 14));
 
         JPasswordField passwordField = new JPasswordField();
-        passwordField.setBounds(275, 290, 300, 45);
+        passwordField.setBounds(350, 290, 300, 45);
 
         JCheckBox passwordCheckBox = new JCheckBox("Näytä salasana");
-        passwordCheckBox.setBounds(272, 340, 150, 20);
+        passwordCheckBox.setBounds(350, 340, 150, 20);
         passwordCheckBox.setBackground(new java.awt.Color(60, 63, 65));
         passwordCheckBox.setForeground(textColor);
 
         JButton loginButton = new JButton();
         loginButton.setBackground(new java.awt.Color(79, 119, 240));
         loginButton.setForeground(textColor);
-        loginButton.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        loginButton.setFont(new java.awt.Font("Segoe UI", 1, 14));
         loginButton.setText("Kirjaudu");
-        loginButton.setBounds(275, 380, 300, 40);
+        loginButton.setBounds(350, 380, 300, 40);
 
         JButton registerButton = new JButton();
         registerButton.setBackground(new java.awt.Color(79, 119, 240));
         registerButton.setForeground(textColor);
-        registerButton.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        registerButton.setFont(new java.awt.Font("Segoe UI", 1, 14));
         registerButton.setText("Uusi käyttäjä? Rekisteröidy");
-        registerButton.setBounds(275, 440, 300, 40);
+        registerButton.setBounds(350, 440, 300, 40);
 
         // Add components to JPanel
         loginPanel.add(titleLabel);
@@ -79,9 +92,9 @@ public class Login extends JFrame {
         loginButton.addActionListener((java.awt.event.ActionEvent evt) -> {
             String user = usernameField.getText();
             String password = passwordField.getText();
-            
+
             if (password.isEmpty() || user.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "Käyttäjätunnus/salasana ei saa olla tyhjä", "Kirjautumisvirhe", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Käyttäjätunnus tai salasana ei saa olla tyhjä", "Kirjautumisvirhe", JOptionPane.ERROR_MESSAGE);
             } else {
                 // Check if username and password are correct
                 if (authentication.authenticateUser(user, password)) {
@@ -94,24 +107,24 @@ public class Login extends JFrame {
             // Focus to username field
             usernameField.requestFocus();
         });
-        
+
         // Set register button functionality
         registerButton.addActionListener((java.awt.event.ActionEvent evt) -> {
             setVisible(false);
             Registration registration = new Registration();
             registration.setVisible(true);
         });
-        
+
         // Make password visible when checkbox is clicked
         passwordCheckBox.addActionListener((ActionEvent e) -> {
             if (passwordCheckBox.isSelected()) {
-                passwordField.setEchoChar((char)0);
+                passwordField.setEchoChar((char) 0);
             } else {
                 passwordField.setEchoChar('*');
             }
         });
     }
-    
+
     // Set visibility of login window
     @Override
     public void setVisible(boolean visible) {
--- src/main/java/ChatApplication/Registration.java
+++ src/main/java/ChatApplication/Registration.java
@@ -1,6 +1,11 @@
 package ChatApplication;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,9 +18,18 @@ public class Registration {
 
     private final JFrame registrationFrame = new JFrame("Registration");
     private final JPanel registrationPanel = new JPanel();
-    
+
     public Registration() {
         initComponents();
+        centeredFrame(registrationFrame);
+    }
+
+    // Center window
+    private void centeredFrame(javax.swing.JFrame jFrame) {
+        Dimension objDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int iCoordX = (objDimension.width - jFrame.getWidth()) / 2;
+        int iCoordY = (objDimension.height - jFrame.getHeight()) / 2;
+        jFrame.setLocation(iCoordX, iCoordY);
     }
 
     private void initComponents() {
@@ -23,7 +37,7 @@ public class Registration {
         Authentication authentication = Authentication.getInstance();
 
         registrationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        registrationFrame.setSize(850, 700);
+        registrationFrame.setSize(1000, 800);
 
         registrationFrame.add(registrationPanel);
         registrationPanel.setLayout(null);
@@ -33,35 +47,49 @@ public class Registration {
         // Create all components for registration window
         JLabel titleLabel = new JLabel("Rekisteröityminen");
         titleLabel.setFont(new java.awt.Font("Dialog", 1, 35));
-        titleLabel.setBounds(275, 20, 400, 100);
+        titleLabel.setBounds(350, 20, 400, 100);
         titleLabel.setForeground(textColor);
 
         JLabel usernameLabel = new JLabel("Käyttäjänimi");
-        usernameLabel.setBounds(275, 190, 100, 25);
+        usernameLabel.setBounds(350, 190, 100, 25);
         usernameLabel.setForeground(textColor);
         usernameLabel.setFont(new java.awt.Font("Segoe UI", 1, 14));
 
         JTextField usernameField = new JTextField();
-        usernameField.setBounds(275, 220, 300, 45);
+        usernameField.setBounds(350, 220, 300, 45);
         usernameField.setFont(new java.awt.Font("Segoe UI", 1, 16));
 
         JLabel passwordLabel = new JLabel("Salasana");
-        passwordLabel.setBounds(275, 265, 100, 25);
+        passwordLabel.setBounds(350, 270, 100, 25);
         passwordLabel.setForeground(textColor);
         passwordLabel.setFont(new java.awt.Font("Segoe UI", 1, 14));
 
-        // TODO Add another password field to verify password
         JPasswordField passwordField = new JPasswordField();
-        passwordField.setBounds(275, 290, 300, 45);
+        passwordField.setBounds(350, 295, 300, 45);
         passwordField.setFont(new java.awt.Font("Segoe UI", 1, 16));
 
-        JLabel emailLabel = new JLabel("Sähköposti");
-        emailLabel.setBounds(275, 335, 100, 25);
+        JLabel passwordLabel2 = new JLabel("Salasana uudelleen");
+        passwordLabel2.setBounds(350, 340, 150, 25);
+        passwordLabel2.setForeground(textColor);
+        passwordLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14));
+
+        JPasswordField passwordField2 = new JPasswordField();
+        passwordField2.setBounds(350, 370, 300, 45);
+        passwordField2.setFont(new java.awt.Font("Segoe UI", 1, 16));
+
+        JLabel passwordCheck = new JLabel("Salasanat eivät täsmää");
+        passwordCheck.setBounds(660, 345, 175, 25);
+        passwordCheck.setFont(new java.awt.Font("Segoe UI", 1, 16));
+        passwordCheck.setForeground(Color.RED);
+        passwordCheck.setVisible(false);
+
+        JLabel emailLabel = new JLabel("Sähköpostiosoite");
+        emailLabel.setBounds(350, 425, 150, 25);
         emailLabel.setForeground(textColor);
         emailLabel.setFont(new java.awt.Font("Segoe UI", 1, 14));
 
         JTextField emailField = new JTextField();
-        emailField.setBounds(275, 360, 300, 45);
+        emailField.setBounds(350, 450, 300, 45);
         emailField.setFont(new java.awt.Font("Segoe UI", 1, 16));
 
         JButton registerButton = new JButton();
@@ -69,35 +97,45 @@ public class Registration {
         registerButton.setForeground(textColor);
         registerButton.setFont(new java.awt.Font("Segoe UI", 1, 14));
         registerButton.setText("Rekisteröidy");
-        registerButton.setBounds(275, 440, 300, 40);
+        registerButton.setBounds(350, 515, 300, 40);
 
         JButton loginButton = new JButton();
         loginButton.setBackground(new java.awt.Color(79, 119, 240));
         loginButton.setForeground(textColor);
         loginButton.setFont(new java.awt.Font("Segoe UI", 1, 14));
         loginButton.setText("Jo käyttäjä? Kirjaudu");
-        loginButton.setBounds(275, 500, 300, 40);
-        
+        loginButton.setBounds(350, 575, 300, 40);
+
         // Add components to JPanel
         registrationPanel.add(titleLabel);
         registrationPanel.add(usernameLabel);
         registrationPanel.add(usernameField);
         registrationPanel.add(passwordLabel);
         registrationPanel.add(passwordField);
+        registrationPanel.add(passwordLabel2);
+        registrationPanel.add(passwordField2);
         registrationPanel.add(emailLabel);
         registrationPanel.add(emailField);
+        registrationPanel.add(passwordCheck);
         registrationPanel.add(loginButton);
         registrationPanel.add(registerButton);
 
         // Add functionality to register button
         registerButton.addActionListener((java.awt.event.ActionEvent evt) -> {
+
             String user = usernameField.getText();
             String password = passwordField.getText();
+            String password2 = passwordField2.getText();
             String email = emailField.getText();
             String nickname = user;
-            
+
             if (password.isEmpty() || user.isEmpty() || email.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "Käyttäjänimi/salasana ei saa olla tyhjä", "Kirjautumisvirhe", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Täytä kaikki tiedot", "Kirjautumisvirhe", JOptionPane.ERROR_MESSAGE);
+            } else if (!password.equals(password2)) {
+                /*passwordCheck.setVisible(true);
+                passwordField.setBackground(new java.awt.Color(251, 93, 93));
+                passwordField2.setBackground(new java.awt.Color(251, 93, 93));*/
+                JOptionPane.showMessageDialog(null, "Salasanat eivät täsmää", "Kirjautumisvirhe", JOptionPane.ERROR_MESSAGE);
             } else {
                 if (!authentication.addUser(user, password, email, nickname)) {
                     JOptionPane.showMessageDialog(null, "Käyttäjänimi on jo rekisteröity", "Kirjautumisvirhe", JOptionPane.ERROR_MESSAGE);
@@ -111,7 +149,7 @@ public class Registration {
             // Focus to username field
             usernameField.requestFocus();
         });
-        
+
         // Add functionality to login button
         loginButton.addActionListener((java.awt.event.ActionEvent evt) -> {
             setVisible(false);
@@ -119,6 +157,13 @@ public class Registration {
             login.setVisible(true);
         });
     }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+        }
+
+    }
+
     // Set visiblity of registration window
     void setVisible(boolean visible) {
         registrationFrame.setVisible(visible);
